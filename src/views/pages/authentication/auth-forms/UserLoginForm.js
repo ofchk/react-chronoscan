199315f7ecc636b1d0d042dd89985dd82a2cfbe5
sarah <sarah@ofchk.biz>
2023-05-10 16:01:33 +0530

    add ldap

---
+++ src/views/pages/authentication/auth-forms/UserLoginForm.js
@@ -0,0 +1,185 @@
+import axiosServices from "utils/axios"
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// material-ui
+import { useTheme } from '@mui/material/styles';
+import {
+    Box,
+    Button,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormHelperText,
+    Grid,
+    IconButton,
+    InputAdornment,
+    InputLabel,
+    OutlinedInput,
+    Typography
+} from '@mui/material';
+
+// third party
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+
+// project imports
+import AnimateButton from 'ui-component/extended/AnimateButton';
+import useAuth from 'hooks/useAuth';
+import useScriptRef from 'hooks/useScriptRef';
+
+import { useNavigate } from 'react-router-dom';
+import axiosServices from "utils/axios"
+
+// assets
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+// ===============================|| JWT LOGIN ||=============================== //
+
+const UserLoginForm = ({ loginProp, ...others }) => {
+    const theme = useTheme();
+
+    const { login } = useAuth();
+    const scriptedRef = useScriptRef();
+    const navigate = useNavigate();
+
+    const [checked, setChecked] = React.useState(true);
+
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Formik
+            initialValues={{
+                // email: 'info@codedthemes.com',
+                // password: '123456',
+                email: '',
+                password: '',
+                submit: null
+            }}
+            validationSchema={Yup.object().shape({
+                password: Yup.string().max(255).required('Password is required')
+            })}
+            onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
+                // navigate('/dashboard');
+                try {
+                    await axiosServices.post(`${API_URL}/user/login`, 
+                        body: {
+                             email: values.email,
+                             password: values.password
+                            }, {
+                        headers: {
+                          "Content-Type": "application/json",
+                        }
+                      })    
+                        .then(data => {
+                          uploadSuccessMessage("File upload completed.");
+                          return false;
+                        })
+                        .catch(error => {
+                          dispatch(updateFileUploadList({
+                            "file_name": file.name,
+                            "progress": 0,
+                            "error": error
+                          }));
+                          existMessage(error);
+                          console.log('Upload axios catch: ', error)
+                        })
+
+                    if (scriptedRef.current) {
+                        setStatus({ success: true });
+                        setSubmitting(false);
+                    }
+                } catch (err) {
+                    console.error(err);
+                    if (scriptedRef.current) {
+                        setStatus({ success: false });
+                        setErrors({ submit: err.message });
+                        setSubmitting(false);
+                    }
+                }
+            }}
+        >
+            {({ errors, handleBlur, handleChange, handleSubmit, isSubmitting, touched, values }) => (
+                <form noValidate onSubmit={handleSubmit} {...others}>
+                    <FormControl fullWidth error={Boolean(touched.email && errors.email)} sx={{ ...theme.typography.customInput }}>
+                        <InputLabel htmlFor="outlined-adornment-email-login">Username</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-email-login"
+                            type="text"
+                            value={values.email}
+                            name="email"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            inputProps={{}}
+                        />
+                        {touched.email && errors.email && (
+                            <FormHelperText error id="standard-weight-helper-text-email-login">
+                                {errors.email}
+                            </FormHelperText>
+                        )}
+                    </FormControl>
+
+                    <FormControl fullWidth error={Boolean(touched.password && errors.password)} sx={{ ...theme.typography.customInput }}>
+                        <InputLabel htmlFor="outlined-adornment-password-login">Password</InputLabel>
+                        <OutlinedInput
+                            id="outlined-adornment-password-login"
+                            type={showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            name="password"
+                            onBlur={handleBlur}
+                            onChange={handleChange}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={handleMouseDownPassword}
+                                        edge="end"
+                                        size="large"
+                                    >
+                                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            inputProps={{}}
+                            label="Password"
+                        />
+                        {touched.password && errors.password && (
+                            <FormHelperText error id="standard-weight-helper-text-password-login">
+                                {errors.password}
+                            </FormHelperText>
+                        )}
+                    </FormControl>
+
+                    {errors.submit && (
+                        <Box sx={{ mt: 3 }}>
+                            <FormHelperText error>{errors.submit}</FormHelperText>
+                        </Box>
+                    )}
+                    <Box sx={{ mt: 2 }}>
+                        <AnimateButton>
+                            <Button color="secondary" disabled={isSubmitting} fullWidth size="large" type="submit" variant="contained">
+                                Sign In
+                            </Button>
+                        </AnimateButton>
+                    </Box>
+                </form>
+            )}
+        </Formik>
+    );
+};
+
+UserLoginForm.propTypes = {
+    loginProp: PropTypes.number
+};
+
+export default UserLoginForm;
