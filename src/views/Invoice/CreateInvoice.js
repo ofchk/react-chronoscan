96e692a6494d15a87b695c8b6dace15f1338901a
sarah <sarah@ofchk.biz>
2023-06-05 16:22:01 +0530

    gl_date fix

--- src/views/Invoice/CreateInvoice.js
+++ src/views/Invoice/CreateInvoice.js
@@ -248,7 +248,8 @@ export default function Create() {
     formData.append('entity_name', entityName );
     formData.append('currency', currencyHeader );
     formData.append('site_id', siteCode );
-    formData.append('gl_date', Moment(glDate).format('D-MMM-YY') );
+    // formData.append('gl_date', Moment(glDate).format('D-MMM-YY') );
+    formData.append('gl_date', gl_date );
     console.log(formData.entries(), param, invoice, iid);
     handleFileUpload(formData, iid, param.name)
     // navigate('/invoice/list')
